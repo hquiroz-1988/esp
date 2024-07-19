b6c3fa5d3cb7c0a3659f664f72016b5ea0063e89
HQLappy <hugo.quiroz92@gmail.com>
2024-07-19 09:39:30 -0700

    - updating config

--- testTasks/main/user_main.c
+++ testTasks/main/user_main.c
@@ -98,7 +98,7 @@ void app_main(void)
     //create a queue to handle gpio event from isr
     gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
     //start gpio task
-    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+    xTaskCreate(gpio_task_example, "T1", 2048, NULL, 10, NULL);
 
     //install gpio isr service
     gpio_install_isr_service(0);
