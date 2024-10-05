19d7ce907d2207c138351430d8a06df3713f85a8
HQLappy <hugo.quiroz92@gmail.com>
2024-10-05 09:20:07 -0700

    - adding test code to test i2c task module

--- solar_panel/main/main.c
+++ solar_panel/main/main.c
@@ -14,11 +14,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/adc.h"
+#include "driver/i2c.h"
 #include "esp_log.h"
 #include "i2c_task.h"
 #include "bus_voltage.h"
 
+
+#define TEST_I2C_TASK
+// #undef  TEST_I2C_TASK
+
 static const char *TAG = "solar panel";
+
 
 
 
@@ -40,6 +46,17 @@ void app_main()
 
         /* print voltage and current */
         ESP_LOGI(TAG, "Voltage: %f, Current: %f\r\n", voltageValue, currentValue);
+
+        /* add test for i2c task here */
+        #ifdef TEST_I2C_TASK
+        int ret;
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        // i2c_master_write_byte(cmd, MPU6050_SENSOR_ADDR << 1 | WRITE_BIT, ACK_CHECK_EN);
+        i2c_master_stop(cmd);
+        ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000 / portTICK_RATE_MS);
+        i2c_cmd_link_delete(cmd);
+        #endif
 
         vTaskDelay(1000 / portTICK_RATE_MS);
     }
