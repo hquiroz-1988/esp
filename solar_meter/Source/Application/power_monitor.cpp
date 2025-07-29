c279b4f2d90eadd1a5d0677dc563766becdff0f5
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-07-28 23:42:00 -0700

    adding methods for queueing bus voltage, current, and power

--- solar_meter/Source/Application/power_monitor.cpp
+++ solar_meter/Source/Application/power_monitor.cpp
@@ -44,6 +44,49 @@ static const char *TAG = "PowerMonitor";
 /*******************************************************************************
  * STATIC FUNCTIONS
  *******************************************************************************/
+Status_t PowerMonitor::queueBusVoltageMessage()
+{
+    Status_t status = busVoltage.getFilteredVoltage(&latestBusVoltage);
+    if (status == STATUS_OKAY)
+    {
+        busVoltageMessage.name = "BusVoltage";
+        busVoltageMessage.timestamp = xTaskGetTickCount();
+        busVoltageMessage.size = sizeof(float);
+        busVoltageMessage.dataPtr = &latestBusVoltage;
+        status = networkingModule.queueNetworkingMessage(&busVoltageMessage);
+    }
+
+    return status;
+}
+
+Status_t PowerMonitor::queueBusCurrentMessage()
+{
+    Status_t status = busCurrent.getFilteredCurrent(&latestBusCurrent);
+    if (status == STATUS_OKAY)
+    {
+        busCurrentMessage.name = "BusCurrent";
+        busCurrentMessage.timestamp = xTaskGetTickCount();
+        busCurrentMessage.size = sizeof(float);
+        busCurrentMessage.dataPtr = &latestBusCurrent;
+        status = networkingModule.queueNetworkingMessage(&busCurrentMessage);
+    }
+
+    return status;
+}
+
+Status_t PowerMonitor::queuePowerMessage()
+{
+    latestPower = latestBusVoltage * latestBusCurrent;
+
+    powerMessage.name = "Power";
+    powerMessage.timestamp = xTaskGetTickCount();
+    powerMessage.size = sizeof(float);
+    powerMessage.dataPtr = &latestPower;
+    
+    return networkingModule.queueNetworkingMessage(&powerMessage);
+}
+
+
 
 /*******************************************************************************
  * GLOBAL FUNCTIONS
