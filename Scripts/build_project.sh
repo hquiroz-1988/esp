ae3dbefe355892e6c6b54c38a132400529b7e86e
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-08-14 00:35:12 -0700

    updating submodules, build  cleans after build

--- Scripts/build_project.sh
+++ Scripts/build_project.sh
@@ -5,5 +5,9 @@ echo "Running ESP8266-RTOS-SDK export script..."
 # this is where we can make our project-specific modifications
 echo ${PWD}
 
+# build the project
 make -C  ./solar_meter all || exit 1
+
+# clean the project
+make -C  ./solar_meter clean || exit 1
 
