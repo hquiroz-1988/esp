c99c9fab2322bbe748bbae5d936ceefd3e4a8c50
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-08-19 00:24:41 -0700

    edit test projects shell scripts to test bus voltage

--- Scripts/test_project.sh
+++ Scripts/test_project.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Running ESP8266-RTOS-SDK test script..."
+echo "Running unit tests..."
 
 # this is where we can make our project-specific modifications
 echo ${PWD}
 
-make -C  ./unit_test gcov || exit 1
+# echo "Testing power monitor" 
+# # Clean project and test power monitor
+# make -C  ./unit_test clean || exit 1
+# make -C ./unit_test all TEST_POWER_MONITOR=true || exit 1
+
+
+echo "Testing BusVoltage"
+# Clean project and test ads1115 module
+make -C  ./unit_test clean || exit 1
+make -C ./unit_test all TEST_BUS_VOLTAGE=true || exit 1
+
+# echo "Testing ADS1115"
+# # Clean project and test ads1115 module
+# make -C  ./unit_test clean || exit 1
+# make -C ./unit_test all TEST_ADS1115=true || exit 1
 
 echo "Running gcovr to generate coverage report..." 
 
