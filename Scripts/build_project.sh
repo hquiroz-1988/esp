c41113012954d94937136a8221d046e0f3120211
Hugo Quiroz <hugo.quiroz92@gmail.com>
2025-08-19 00:02:13 -0700

    Implement ADS1115 Interface (#32)
    * adding necessary header interface attributes to ads1115.hpp
    
    * reverting removal of old enums
    
    * naming new enum Operational status to differntiate between new and old
    
    * sneaking in a clean project before building into bash script

--- Scripts/build_project.sh
+++ Scripts/build_project.sh
@@ -5,6 +5,9 @@ echo "Running ESP8266-RTOS-SDK export script..."
 # this is where we can make our project-specific modifications
 echo ${PWD}
 
+# clean the project before running
+make -C  ./solar_meter clean || exit 1
+
 # build the project
 make -C  ./solar_meter all || exit 1
 
