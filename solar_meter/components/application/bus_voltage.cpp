d9bcfc8a18d1a9ba05d551aff85a276a569f64c9
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-08-25 00:47:13 -0700

    implementing bus voltage cpp

--- solar_meter/components/application/bus_voltage.cpp
+++ solar_meter/components/application/bus_voltage.cpp
@@ -26,7 +26,7 @@ extern "C"
 /*******************************************************************************
  * PRIVATE MACROS AND DEFINES
  *******************************************************************************/
-static const char *TAG = "bus voltage";
+static const char *TAG = "BusVoltage";
 
 /*******************************************************************************
  * PRIVATE TYPEDEFS
@@ -51,47 +51,73 @@ static const char *TAG = "bus voltage";
 /*******************************************************************************
  * GLOBAL FUNCTIONS
  *******************************************************************************/
-BusVoltage::BusVoltage(PowerMonitor & _pm, ADS1115 & _ads1115) : pm(_pm), ADS1115Channel(_ads1115)
+BusVoltage::BusVoltage(ADS1115 & _ads1115) 
+    : ADS1115Channel(_ads1115), pm(nullptr)
 {
     // Constructor implementation
 }
 
-Status_t BusVoltage::init(void)
+BusVoltage::~BusVoltage()
 {
+    // Destructor implementation
+}
 
-    /* for configuration we are not trying to trigger a conversion+ */
-    configRegister.opStatus = ADS1115_OperationalStatus_t::Write_No_Effect;
-    /* busvoltage measurement will be single ended at AIN0  */
-    configRegister.mux = ADS1115Mux_t::AIN0_GND;
-    /*  range 2.048 will be used since device VDD will be 3.3V, 2.048 is the
-        next largest value    */
-    configRegister.pga = ADS1115PGA_t::FSR_2_048V;
-    /* all measurements are requested not automatic, continuous would drain power */
-    configRegister.mode = ADS1115Mode_t::SingleShot;
-    /*  sampling rate will be high but actual sampling will be much lower rate
-        this is to save power, lower device sampling rates require device to be on much
-        longer hence consuming more power */
-    configRegister.dataRate = ADS1115DataRate_t::SPS_860;
-    /*  comparator mode might need to window when measuring battery voltage to ensure
-        batteries stay above and below thresholds    */
-    configRegister.compMode = ADS1115CompMode_t::Window;
-    /* comparator polarity will be active low */
-    configRegister.compPolarity = ADS1115CompPolarity_t::ActiveLow;
-    /* comparator is latching, in case we want to poll signal   */
-    configRegister.compLatch = ADS1115CompLatch_t::Latching;
-    /* asserting alert/rdy pin after four coversions to filter out sporadic measurements */
-    configRegister.compQueue = ADS1115CompQueue_t::AssertAfterFourConversions;
+Status_t BusVoltage::initialize(PowerMonitor * _pm)
+{
+    /* check that pm is not nullptr */
+    Status_t retVal = STATUS_OKAY;
+
+    if(_pm != nullptr)
+    {
+        pm = _pm;
+    }
+    else
+    {
+        retVal = STATUS_NULL_POINTER;
+    }
 
     /* configure ADS1115 module */
-    Status_t retVal = ads1115.configure(configRegister);
+    if(retVal == STATUS_OKAY)
+    {
+        /* for configuration we are not trying to trigger a conversion+ */
+        configRegister.opStatus = ADS1115_OperationalStatus_t::Write_No_Effect;
+        /* busvoltage measurement will be single ended at AIN0  */
+        configRegister.mux = ADS1115Mux_t::AIN0_GND;
+        /*  range 2.048 will be used since device VDD will be 3.3V, 2.048 is the
+            next largest value    */
+        configRegister.pga = ADS1115PGA_t::FSR_2_048V;
+        /* all measurements are requested not automatic, continuous would drain power */
+        configRegister.mode = ADS1115Mode_t::SingleShot;
+        /*  sampling rate will be high but actual sampling will be much lower rate
+            this is to save power, lower device sampling rates require device to be on much
+            longer hence consuming more power */
+        configRegister.dataRate = ADS1115DataRate_t::SPS_860;
+        /*  comparator mode might need to window when measuring battery voltage to ensure
+            batteries stay above and below thresholds    */
+        configRegister.compMode = ADS1115CompMode_t::Window;
+        /* comparator polarity will be active low */
+        configRegister.compPolarity = ADS1115CompPolarity_t::ActiveLow;
+        /* comparator is latching, in case we want to poll signal   */
+        configRegister.compLatch = ADS1115CompLatch_t::Latching;
+        /* asserting alert/rdy pin after four coversions to filter out sporadic measurements */
+        configRegister.compQueue = ADS1115CompQueue_t::AssertAfterFourConversions;
+        retVal = ads1115.configure(configRegister);
+    }
 
     /*  set low and high thresholds for bus voltage, these will be 
         set to not use the comparator but instad used to alert when
         a sample is ready this is done by setting low high threshold value to most
         negative value possible, thresholds will be set at conversion request since
         different channels might actually use the threshold values  */
-    lowThreshold = ADS1115_CONVERSION_COMPLETE_LO;
-    highThreshold = ADS1115_CONVERSION_COMPLETE_HI;
+    if(retVal == STATUS_OKAY)
+    {
+        retVal = setLowThreshold(ADS1115_CONVERSION_COMPLETE_LO);
+    }
+
+    if(retVal == STATUS_OKAY)
+    {
+        retVal = setHighThreshold(ADS1115_CONVERSION_COMPLETE_HI);
+    }
 
     /* init conversion object */
     type = Conversion_t::SingleEnded;
@@ -109,9 +135,14 @@ Status_t BusVoltage::init(void)
 
  void BusVoltage::runAlertISR(void * arg)
  {
-    // Handle the alert interrupt, e.g., notify a task or set a flag
-    // This is a placeholder implementation
-    ESP_LOGI(TAG, "Alert ISR triggered");
-    //!TODO: call power monitor task notify
-    pm.notifyFromISR();
+    if(pm != nullptr)
+    {
+        PowerMonitor & powerMonitor = (*pm);
+        powerMonitor.notifyFromISR(arg);
+    }
+    else
+    {
+        /* perform something if we have nullptr */
+        ESP_LOGE(TAG,"PowerMonitor is nullptr\n");
+    }
  }
