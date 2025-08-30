47ee5845b8aee7c702e9c3283d43c39f89a3f21b
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-08-29 18:58:45 -0700

    running a real clean before tests instead of before each test

--- Scripts/test_project.sh
+++ Scripts/test_project.sh
@@ -5,14 +5,14 @@ echo "Running unit tests..."
 # this is where we can make our project-specific modifications
 echo ${PWD}
 
+make -C  ./unit_test realclean || exit 1
+
 echo "Testing PowerMonitor" 
 # Clean project and test power monitor
-make -C  ./unit_test clean || exit 1
 make -C ./unit_test gcov TEST_POWER_MONITOR=true || exit 1
 
 echo "Testing BusVoltage"
 # Clean project and test ads1115 module
-make -C  ./unit_test clean || exit 1
 make -C ./unit_test gcov TEST_BUS_VOLTAGE=true || exit 1
 
 # echo "Testing ADS1115"
