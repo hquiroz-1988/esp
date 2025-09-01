d7534c1e40f9a63288a9221bfcd472721ca0767f
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-09-01 11:51:56 -0700

    create i2c bus header

---
+++ solar_meter/components/peripheral_drivers/include/i2c_bus.hpp
@@ -0,0 +1,63 @@
+/**
+ *******************************************************************************
+ * @file    i2c_bus.hpp
+ * @author  HQ
+ * @date    2025-08-31 22:40:46
+ * @brief   
+ *******************************************************************************
+ */
+
+#ifndef I2C_BUS_HPP
+#define I2C_BUS_HPP
+
+/*******************************************************************************
+ * INCLUDES
+*******************************************************************************/
+#include "i2c.h"
+#include "gpio.hpp"
+
+/*******************************************************************************
+ * MACROS AND DEFINES
+*******************************************************************************/
+
+/*******************************************************************************
+ * TYPEDEFS
+*******************************************************************************/
+class I2CBus
+{
+    friend class I2CDevice;
+
+public:
+    static const int MAX_DEV_COUNT = 4;
+    I2CBus(Gpio & sda, Gpio & scl, uint32_t i2c_clock_speed);
+    virtual ~I2CBus();
+    void initialize(std::vector<I2CDevice *> device_list);
+
+protected:
+    bool acquire(int dev_id, bool auto_cs = true);
+    bool release(int dev_id, bool auto_cs = true);
+    bool transmit(uint8_t *data, uint16_t size, uint32_t timeout);
+    bool transceive(uint8_t *tx_data, uint8_t *rx_data, uint16_t size,
+                    uint32_t timeout);
+    bool isReadyToSend();
+
+private:
+    I2C_HandleTypeDef &_hi2c;
+    const uint32_t i2c_clk_freq;
+    int cur_dev;
+
+    I2CDeviceInfo devices[MAX_DEV_COUNT];
+
+    // void HAL_I2C_TxCpltCallback(I2C_HandleTypeDef *hi2c);
+};
+
+/*******************************************************************************
+ * EXPORTED VARIABLES
+*******************************************************************************/
+
+/*******************************************************************************
+ * GLOBAL FUNCTION PROTOTYPES
+*******************************************************************************/
+
+
+#endif // I2C_BUS_HPP
