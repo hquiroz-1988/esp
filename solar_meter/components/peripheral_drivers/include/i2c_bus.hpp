1f79c8fd32f7ade76a97dc63dc7efedeb5bfcfa8
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2025-12-20 00:47:15 -0800

    - i2c bus edits

--- solar_meter/components/peripheral_drivers/include/i2c_bus.hpp
+++ solar_meter/components/peripheral_drivers/include/i2c_bus.hpp
@@ -3,7 +3,7 @@
  * @file    i2c_bus.hpp
  * @author  HQ
  * @date    2025-08-31 22:40:46
- * @brief   
+ * @brief
  *******************************************************************************
  */
 
@@ -12,7 +12,7 @@
 
 /*******************************************************************************
  * INCLUDES
-*******************************************************************************/
+ *******************************************************************************/
 #include "driver/i2c.h"
 #include "gpio.hpp"
 #include "typedefs.h"
@@ -22,31 +22,32 @@
 
 /*******************************************************************************
  * MACROS AND DEFINES
-*******************************************************************************/
+ *******************************************************************************/
 
 /*******************************************************************************
  * TYPEDEFS
-*******************************************************************************/
+ *******************************************************************************/
 
 /**
  * @brief I2C transfer acknowledgment type
  */
-enum class I2CTransferAckType_t 
+enum class I2CTransferAckType_t
 {
-    MASTER_ACK          = I2C_MASTER_ACK,                /*!< I2C ack for each byte read */
-    MASTER_NACK         = I2C_MASTER_NACK,              /*!< I2C nack for each byte read */
-    MASTER_LAST_NACK    = I2C_MASTER_LAST_NACK,    /*!< I2C nack for the last byte*/
-    MASTER_ACK_MAX      = I2C_MASTER_ACK_MAX,
+    MASTER_ACK = I2C_MASTER_ACK,             /*!< I2C ack for each byte read */
+    MASTER_NACK = I2C_MASTER_NACK,           /*!< I2C nack for each byte read */
+    MASTER_LAST_NACK = I2C_MASTER_LAST_NACK, /*!< I2C nack for the last byte*/
+    MASTER_ACK_MAX = I2C_MASTER_ACK_MAX,
 };
 
 typedef struct
 {
-    uint8_t * data;
+    uint8_t *data;
     size_t size;
+    uint8_t devAddr;
+    uint8_t regAddr;
     bool ackEn;
     I2CTransferAckType_t ackType;
 } I2CTransfer_t;
-
 
 class I2CBus
 {
@@ -54,14 +55,14 @@ class I2CBus
 
 public:
     static const int MAX_DEV_COUNT = 4;
-    I2CBus(Gpio & _sda, Gpio & _scl, i2c_port_t _port);
+    I2CBus(Gpio &_sda, Gpio &_scl, i2c_port_t _port);
     virtual ~I2CBus();
     /**
      * @brief Initializes the I2C bus with the current configuration.
-     * 
+     *
      * This method installs and initializes the i2c driver, and subsequent
      * calls to this method will result in error.
-     * 
+     *
      * @return Status_t
      */
     Status_t initialize(void);
@@ -72,39 +73,39 @@ public:
 protected:
     Status_t acquire(int dev_id, uint32_t timeout);
     Status_t release(int dev_id);
-    Status_t transmit(I2CTransfer_t & transfer);
-    Status_t receive(I2CTransfer_t & transfer);
+    Status_t write(I2CTransfer_t &transfer);
+    Status_t read(I2CTransfer_t &transfer);
 
 private:
-    Gpio & sda;
-    Gpio & scl;
+    Gpio &sda;
+    Gpio &scl;
     i2c_port_t port;
     i2c_config_t conf;
     /* clock speeds above 400khz must be performed using I2C High-Speed mode */
     uint32_t clockStretching;
     uint32_t devItr;
-    I2CDevice * devices[MAX_DEV_COUNT];
+    I2CDevice *devices[MAX_DEV_COUNT];
     Mutex busMutex;
     int currDevID = -1;
     i2c_cmd_handle_t cmdHandle = nullptr;
 
     Status_t installDriver(void);
-    Status_t configureDriver(void); 
+    Status_t configureDriver(void);
     Status_t createLink(void);
-    Status_t masterStart(void);
-    Status_t masterWrite(I2CTransfer_t & transfer);
-    Status_t masterRead(I2CTransfer_t & transfer);
-    Status_t masterStop(void);
-    
+    Status_t deleteLink(void);
+    Status_t cmdStart(void);
+    Status_t cmdBuild(I2CTransfer_t &transfer, bool write);
+    Status_t masterRead(I2CTransfer_t &transfer);
+    Status_t cmdSend(void);
+    Status_t cmdEnd(void);
 };
 
 /*******************************************************************************
  * EXPORTED VARIABLES
-*******************************************************************************/
+ *******************************************************************************/
 
 /*******************************************************************************
  * GLOBAL FUNCTION PROTOTYPES
-*******************************************************************************/
-
+ *******************************************************************************/
 
 #endif // I2C_BUS_HPP
