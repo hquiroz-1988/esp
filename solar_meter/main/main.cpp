97ca62e2fb7bb8e951342d05d59e2f7c7a16e0dd
hugo-wsl-ubuntu-24.04 <hugo.quiroz92@gmail.com>
2026-03-10 23:31:16 -0700

    - changes to main

--- solar_meter/main/main.cpp
+++ solar_meter/main/main.cpp
@@ -38,10 +38,18 @@ extern "C" void app_main()
     Gpio sclPin(GpioPin::GPIO_5, GpioMode::OutputOpenDrain, GpioPullup::Enable);
     I2CBus i2cBus(sdaPin, sclPin, I2C_NUM_0);
 
-    /* initialize ads1115 */
+    /* create alert pin for ads1115 */
     Gpio ads1115AlertPin(GpioPin::GPIO_6, GpioMode::Input, GpioPullup::Disable, GpioIntrType::FallingEdge);
+
+    /* create ads1115 instance */
     ADS1115 ads1115(ads1115AlertPin);
-    
+
+    /* add ads1115 to list of I2C devices */
+    //!TODO: add status check to see if device was added successfully 
+    i2cBus.addDevice(&ads1115);
+
+    /* once ads1115 to list of devices we can initialize registers */
+    ads1115.initialize();
 
     // BusVoltage busVoltage(ads1115);
 
